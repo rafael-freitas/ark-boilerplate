489b3b2ad5100958b06275577813404873da9d4c
Rafael Freitas <mail@rafael.to>
2016-08-09 17:29:18 -0300

    initial commit

--- config/env/test.js
+++ config/env/test.js
@@ -1,63 +1,63 @@
-'use strict';
+'use strict'
 
 module.exports = {
-    db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
-    http: {
-        port: 3001
+  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-test',
+  http: {
+    port: 3001
+  },
+  aggregate: false,
+  assets: {
+    hash: false
+  },
+  logging: {
+    format: 'common'
+  },
+  app: {
+    name: 'MEAN - A Modern Stack - Test'
+  },
+  strategies: {
+    local: {
+      enabled: true
     },
-    aggregate: false,
-    assets: {
-        hash: false
+    landingPage: '/',
+    facebook: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      enabled: false
     },
-    logging: {
-        format: 'common'
+    twitter: {
+      clientID: 'CONSUMER_KEY',
+      clientSecret: 'CONSUMER_SECRET',
+      callbackURL: 'http://localhost:3000/auth/twitter/callback',
+      enabled: false
     },
-    app: {
-        name: 'MEAN - A Modern Stack - Test'
+    github: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/github/callback',
+      enabled: false
     },
-    strategies: {
-        local: {
-            enabled: true
-        },
-        landingPage: '/',
-        facebook: {
-            clientID: 'APP_ID',
-            clientSecret: 'APP_SECRET',
-            callbackURL: 'http://localhost:3000/auth/facebook/callback',
-            enabled: false
-        },
-        twitter: {
-            clientID: 'CONSUMER_KEY',
-            clientSecret: 'CONSUMER_SECRET',
-            callbackURL: 'http://localhost:3000/auth/twitter/callback',
-            enabled: false
-        },
-        github: {
-            clientID: 'APP_ID',
-            clientSecret: 'APP_SECRET',
-            callbackURL: 'http://localhost:3000/auth/github/callback',
-            enabled: false
-        },
-        google: {
-            clientID: 'APP_ID',
-            clientSecret: 'APP_SECRET',
-            callbackURL: 'http://localhost:3000/auth/google/callback',
-            enabled: false
-        },
-        linkedin: {
-            clientID: 'API_KEY',
-            clientSecret: 'SECRET_KEY',
-            callbackURL: 'http://localhost:3000/auth/linkedin/callback',
-            enabled: false
-        }
+    google: {
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+      enabled: false
     },
-    emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
-    mailer: {
-        service: 'SERVICE_PROVIDER',
-        auth: {
-            user: 'EMAIL_ID',
-            pass: 'PASSWORD'
-        }
-    },
-    secret: 'SOME_TOKEN_SECRET'
-};
+    linkedin: {
+      clientID: 'API_KEY',
+      clientSecret: 'SECRET_KEY',
+      callbackURL: 'http://localhost:3000/auth/linkedin/callback',
+      enabled: false
+    }
+  },
+  emailFrom: 'SENDER EMAIL ADDRESS', // sender address like ABC <abc@example.com>
+  mailer: {
+    service: 'SERVICE_PROVIDER',
+    auth: {
+      user: 'EMAIL_ID',
+      pass: 'PASSWORD'
+    }
+  },
+  secret: 'SOME_TOKEN_SECRET'
+}
